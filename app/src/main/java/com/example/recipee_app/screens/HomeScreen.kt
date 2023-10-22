28e1d804d86f50811ac331ca9ce0879c8e58bd4f
Arnaud <arnaudduhamel2@gmail.com>
2023-10-22 22:58:29 +0200

    add: main page

--- app/src/main/java/com/example/recipee_app/screens/HomeScreen.kt
+++ app/src/main/java/com/example/recipee_app/screens/HomeScreen.kt
@@ -1,14 +1,26 @@
 package com.example.recipee_app.screens
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.recipee_app.R
 
 @Composable
 fun HomeScreen(navController: NavController) {
@@ -18,6 +30,37 @@ fun HomeScreen(navController: NavController) {
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally
     ){
+        Spacer(
+            modifier = Modifier
+                .fillMaxHeight(0.025f)
+        )
+        Image(
+            painter = painterResource(id = R.drawable.gordon_ramsay_portrait),
+            contentDescription = stringResource(R.string.portrait_picture_of_gordon_ramsay),
+            modifier = Modifier
+                .fillMaxWidth(0.95f)
+        )
+        Spacer(
+            modifier = Modifier
+                .fillMaxHeight(0.03f)
+        )
+        Button(
+            shape = MaterialTheme.shapes.medium,
+            modifier = Modifier
+                .padding(top = 5.dp)
+                .fillMaxWidth(0.925f),
+            onClick = {
 
+            }
+        ) {
+            Text(
+                text = "Gordon Ramsay",
+                modifier = Modifier
+                    .padding(top = 5.dp,
+                            bottom = 9.dp),
+                fontWeight = FontWeight.Bold,
+                fontSize = 30.sp
+            )
+        }
     }
 }
