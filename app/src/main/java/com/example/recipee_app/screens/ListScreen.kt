9a05b91b484198bbc3420184d60f4ccb955f48a3
Arnaud <arnaudduhamel2@gmail.com>
2023-10-23 20:03:24 +0200

    add: application class

---
+++ app/src/main/java/com/example/recipee_app/screens/ListScreen.kt
@@ -0,0 +1,9 @@
+package com.example.recipee_app.screens
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun ListScreen(navController: NavController) {
+
+}
