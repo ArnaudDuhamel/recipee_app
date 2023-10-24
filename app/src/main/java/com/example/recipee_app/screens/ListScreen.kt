32a6c57ff30d805a5420bc884281a73454906097
Arnaud <arnaudduhamel2@gmail.com>
2023-10-24 13:43:54 +0200

    add: list page

--- app/src/main/java/com/example/recipee_app/screens/ListScreen.kt
+++ app/src/main/java/com/example/recipee_app/screens/ListScreen.kt
@@ -1,6 +1,10 @@
 package com.example.recipee_app.screens
 
+import android.app.Activity
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -8,7 +12,15 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.selection.toggleable
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -20,35 +32,131 @@ import androidx.navigation.NavController
 import com.example.recipee_app.AppClass
 import com.example.recipee_app.R
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.TextUnit
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ListScreen(navController: NavController, model: AppClass) {
-    LazyColumn(
-        horizontalAlignment = Alignment.Start
-    ) {
-        // here the notesList is changed into a list because the itemsIndexed
-        // function takes a list
-        itemsIndexed(model.getRecipesList()) { _, pair ->
-            Row(
-                modifier = Modifier
-                    .height(50.dp),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                // This text object contains the list item itself
-                Image(
-                    painter = painterResource(id = pair.first),
-                    contentDescription = pair.second + " picture"
-                )
-                // The checkbox. Great care was take to look as much as possible
-                // as the presented sketch
-                Text(
-                    text = pair.second
-                )
-            }
 
-            HorizontalDivider(
-                thickness = 1.dp
+    val view = LocalView.current
+    val window = (view.context as Activity).window
+
+    val focusManager = LocalFocusManager.current
+
+    val inputValue = remember { mutableStateOf(TextFieldValue()) }
+
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(
+                        text = "Gordon's food list",
+                        fontWeight = FontWeight.Bold,
+                    )
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    Color(window.statusBarColor)
+                ),
+                navigationIcon = {
+                    IconButton(
+                        onClick = { print("hello") }) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "Localized description"
+                        )
+                    }
+                }
+                )
+        },
+        modifier = Modifier
+            .fillMaxWidth()
+    ) { paddingValues ->
+
+        Column(
+            verticalArrangement = Arrangement.Top,
+            modifier = Modifier.padding(paddingValues)
+        ) {
+            TextField(
+                value = inputValue.value,
+                onValueChange = {
+                    inputValue.value = it
+                },
+                modifier = Modifier.fillMaxWidth(),
+                keyboardOptions = KeyboardOptions(
+                    capitalization = KeyboardCapitalization.None,
+                    autoCorrect = true,
+                    keyboardType = KeyboardType.Text,
+                    imeAction = ImeAction.Done
+                ),
+                textStyle = TextStyle(
+                    color = Color.Black,
+                    fontSize = TextUnit.Unspecified,
+                    fontFamily = FontFamily.SansSerif,
+                    textAlign = TextAlign.Left
+                ),
+                maxLines = 1,
+                singleLine = true,
+                keyboardActions = KeyboardActions(onDone = {
+                    focusManager.clearFocus()
+                }),
+                trailingIcon = {
+                    Icon(
+                        Icons.Filled.Search,
+                        contentDescription = "Search"
+                    )
+                }
             )
+
+            LazyColumn(
+                horizontalAlignment = Alignment.Start
+            ) {
+
+                // here the notesList is changed into a list because the itemsIndexed
+                // function takes a list
+                itemsIndexed(model.getRecipesList()) { _, pair ->
+                    Row(
+                        modifier = Modifier
+                            .height(100.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        // This text object contains the list item itself
+                        Image(
+                            painter = painterResource(id = pair.first),
+                            contentDescription = pair.second + " picture",
+                            modifier = Modifier.fillMaxWidth(0.25f)
+                        )
+                        // The checkbox. Great care was take to look as much as possible
+                        // as the presented sketch
+                        Text(
+                            text = pair.second
+                        )
+                    }
+
+                    HorizontalDivider(
+                        thickness = 1.dp
+                    )
+                }
+            }
         }
     }
 }
